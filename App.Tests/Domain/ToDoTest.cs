1c070765ea1d3aa25dc124d6cad2f6d8920b811f
enzo <enzo.bonansea@gmail.com>
2023-03-29 17:04:16 -0300

    test(ToDo): it must have non empty description

--- App.Tests/Domain/ToDoTest.cs
+++ App.Tests/Domain/ToDoTest.cs
@@ -7,17 +7,22 @@ public class ToDoTest
     [Fact]
     public void ItMustHaveNonEmptyTitle()
     {
-        var creation = () => new ToDo("");
+        var creation = () => new ToDo("", "");
         var exception = Assert.Throws<ArgumentException>(creation);
         Assert.Equal(ToDo.TitleMustBeNonEmptyErrorMessage, exception.Message);
     }
 
     [Fact]
-    public void ItMustHaveNonEmptyDescription(){}
+    public void ItMustHaveNonEmptyDescription()
+    {
+        var creation = () => new ToDo("Some title", "");
+        var exception = Assert.Throws<ArgumentException>(creation);
+        Assert.Equal(ToDo.DescriptionMustBeNonEmptyErrorMessage, exception.Message);
+    }
 
     [Fact]
-    public void ItMustHaveAFutureDate(){}
+    public void ItMustHaveAFutureDate() { }
 
     [Fact]
-    public void ItMustEmitToDoCreatedEvent(){}
+    public void ItMustEmitToDoCreatedEvent() { }
 }
