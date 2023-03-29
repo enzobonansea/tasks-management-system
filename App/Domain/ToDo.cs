9fb261bf1a535f61bb1cdac2d6a5dd08936e971d
enzo <enzo.bonansea@gmail.com>
2023-03-29 17:04:38 -0300

    feat(ToDo): requiring non empty description

--- App/Domain/ToDo.cs
+++ App/Domain/ToDo.cs
@@ -1,13 +1,19 @@
 namespace app.Domain;
 
-public class ToDo {
+public class ToDo
+{
     public const string TitleMustBeNonEmptyErrorMessage = "Title most be non empty";
+    public const string DescriptionMustBeNonEmptyErrorMessage = "Description must be non empty";
 
     private readonly string title;
-    
-    public ToDo(string title) {
+    private readonly string description;
+
+    public ToDo(string title, string description)
+    {
         if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException(ToDo.TitleMustBeNonEmptyErrorMessage);
-        
+        if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException(ToDo.TitleMustBeNonEmptyErrorMessage);
+
         this.title = title;
+        this.description = description;
     }
 }
