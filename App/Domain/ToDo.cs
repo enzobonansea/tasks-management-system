77a092ae8fcc10b56c6deb4b56ce8fd1c1d12856
enzo <enzo.bonansea@gmail.com>
2023-03-29 17:11:03 -0300

    feat(ToDo): date must be future

--- App/Domain/ToDo.cs
+++ App/Domain/ToDo.cs
@@ -8,13 +8,16 @@ public class ToDo
 
     private readonly string title;
     private readonly string description;
+    private readonly DateTime date;
 
     public ToDo(string title, string description, DateTime date)
     {
         if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException(ToDo.TitleMustBeNonEmptyErrorMessage);
         if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException(ToDo.DescriptionMustBeNonEmptyErrorMessage);
+        if (date < DateTime.Now) throw new ArgumentException(ToDo.DateMustBeFutureErrorMessage);
 
         this.title = title;
         this.description = description;
+        this.date = date;
     }
 }
